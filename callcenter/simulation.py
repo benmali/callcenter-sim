cfc1040863f2eed080cdb6ce918647bddf63f7b0
Ben <benmali94@gmail.com>
2022-03-31 00:47:44 +0300

    added function mapper

--- callcenter/simulation.py
+++ callcenter/simulation.py
@@ -10,6 +10,7 @@ class CallCenterSimulation:
         self.events = []
         self.curr_date = TimeHelper.string_to_date('01-01-2021')
         self.curr_hour = '08:00'
+        self.closing_hour = '20:00'
         self.curr_time = None  # This needs to be the curr time and the curr date
         self.mode = "PriorityQueue"
         self.call_center = callcenter.CallCenter(self.mode)
@@ -20,6 +21,14 @@ class CallCenterSimulation:
         self.n_end_clients = 1000
         self.n_employees_by_sector = {"High-Tech": 0, "Blue-Collar": 0}  # Map the number of employees by sector
         self.companies = []  # This list only grows along iteration
+        self.event_mapping = {
+            'incoming_call': self.incoming_call,
+            'end_call': self.end_call,
+            'end_chat': self.end_chat,
+            'sign_new_company': self.sign_new_company,
+            'sign_new_restaurant': self.sign_new_restaurant,
+            'agent_break': self.agent_break
+        }
 
         # The day type (rainy w/e) is in charge of where is my food calls/chat
         # Sector and age are in charge of reset my password/ login issues/ cant process my order cause of rules
@@ -90,29 +99,13 @@ class CallCenterSimulation:
             np.random.seed(i+1)
             self.sign_new_company()
             client = self.gen_client()
-            hpq.heappush(self.events, callcenter.Event(client.arrival_time, "arriving", client))
-            hpq.heappush(self.events, callcenter.Event(self.curr_time, "door open", client))
-            while self.curr_time < self.simulation_time:
+            hpq.heappush(self.events, callcenter.Event(client.arrival_time, "incoming_call", client))
+
+            while self.curr_hour < self.closing_hour:
                 event = hpq.heappop(self.events)
                 self.curr_time = event.time
-                if event.event_type == "arriving":
-                    self.arriving(event)
-                elif event.event_type == "door open":
-                    self.door_open(event)
-                elif event.event_type == "elevator fix":
-                    self.elevator_fix(event)
-                elif event.event_type == "door close":
-                    self.door_close(event)
-            for floor in self.floors:
-                for client in floor.line:
-                    if (self.curr_time - client.arrival_time) > 15 * 60 and not client.got_service:
-                        self.abandoned += 1
-            avg_cap = list(map(lambda x: x / (self.curr_time - 21600), self.elevators_avg_cap))
-            self.abandoned_lst.append(self.abandoned)
-            for key, value in self.service_dist.items():
-                self.service_times[key] += value
-            for j in range(len(avg_cap)):
-                self.elevator_mat[i][j] = avg_cap[j]
+                handle_event = self.event_mapping.get(event.event_type)
+                handle_event(event)
 
 
 
